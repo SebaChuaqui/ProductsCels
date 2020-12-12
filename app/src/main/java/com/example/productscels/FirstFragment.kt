609e2200f6aa48dde61894f33ac8d2d8578b8022
Seba <pezkaos@gmail.com>
2020-12-12 10:20:42 -0600

    Fragmentos

--- app/src/main/java/com/example/productscels/FirstFragment.kt
+++ app/src/main/java/com/example/productscels/FirstFragment.kt
@@ -1,31 +1,57 @@
 package com.example.productscels
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.productscels.ViewModel.MyViewModel
+import com.example.productscels.adapter.AdapterProducts
+import com.example.productscels.pojos.ProductsItem
+import kotlinx.android.synthetic.main.fragment_first.*
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+
+class FirstFragment : Fragment(), AdapterProducts.ProductsId {
+
+    lateinit var  mProductsViewModel: MyViewModel
+    var mId: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mProductsViewModel= ViewModelProvider(this).get(MyViewModel::class.java)
+    }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val mRecyclerView = recyclerView
+        val mAdapter = AdapterProducts(this)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        mRecyclerView.adapter = mAdapter
+        mRecyclerView.layoutManager = GridLayoutManager(context, 2)
+
+        mProductsViewModel.mAllProducts.observe(viewLifecycleOwner, Observer{
+            mAdapter.updateListProducts(it)
+            Log.d("funciona", it.toString())
+        })
+
+    }
+
+    override fun passData(mProductId: ProductsItem) {
+        val mBundle = Bundle()
+        mBundle.putInt("id", mProductId.id)
+        findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, mBundle)
     }
 }
